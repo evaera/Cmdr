60077450ee765e4e6e546e34d32d950013c960f6
Eryn Lynn <eryn@eryn.io>
2018-09-18 02:33:20 -0400

    Remove useless statement

--- Cmdr/BuiltInCommands/announceServer.lua
+++ Cmdr/BuiltInCommands/announceServer.lua
@@ -1,6 +1,5 @@
 return function (context, text)
 	context:BroadcastEvent("Message", ("[Announcement] %s"):format(text))
-	context:Reply("Bluh bli", Color3.fromRGB(0, 0, 255))
 
 	return "Created announcement."
 end
