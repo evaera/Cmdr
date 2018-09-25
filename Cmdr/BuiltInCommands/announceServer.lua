8e78e1f649cf1e96aaa6db2a767dba74894c015f
Eryn Lynn <eryn@eryn.io>
2018-09-24 22:52:28 -0400

    Make Message event send player

--- Cmdr/BuiltInCommands/announceServer.lua
+++ Cmdr/BuiltInCommands/announceServer.lua
@@ -1,5 +1,5 @@
 return function (context, text)
-	context:BroadcastEvent("Message", ("[Announcement] %s"):format(text))
+	context:BroadcastEvent("Message", text, context.Executor)
 
 	return "Created announcement."
 end
--- Cmdr/CmdrClient/DefaultEventHandlers.lua
+++ Cmdr/CmdrClient/DefaultEventHandlers.lua
@@ -4,7 +4,7 @@ local Window = require(script.Parent.CmdrInterface.Window)
 return function (Cmdr)
 	Cmdr:HandleEvent("Message", function (text)
 		StarterGui:SetCore("ChatMakeSystemMessage", {
-			Text = text;
+			Text = ("[Announcement] %s"):format(text);
 			Color = Color3.fromRGB(249, 217, 56);
 		})
 	end)
