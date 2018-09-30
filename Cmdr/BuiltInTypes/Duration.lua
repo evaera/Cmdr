e3bfc33ee21c3240dae660732821b4ac80e0ccc8
Michael Haas <TripleFrequency@TripleFrequency.com>
2018-09-29 21:23:18 -0400

    Month/Minute ambiguity defaults to Minute

--- Cmdr/BuiltInTypes/Duration.lua
+++ Cmdr/BuiltInTypes/Duration.lua
@@ -17,12 +17,12 @@ end
 local unitFinder = Util.MakeFuzzyFinder(searchKeyTable)
 
 local function stringToSecondDuration(stringDuration)
-    -- The duration cannot be null or an empty string
+    -- The duration cannot be null or an empty string.
     if stringDuration == nil or stringDuration == "" then
         return nil, stringDuration
     end
-    -- The duration must end with a unit.
-    -- If it doesn't we return true as the third value to indicate we need to offer autocomplete for units.
+    -- The duration must end with a unit,
+    -- if it doesn't then return true as the third value to indicate the need to offer autocomplete for units.
     if stringDuration:find("^.*%d+$") then
         return nil, stringDuration, true
     end
@@ -32,15 +32,14 @@ local function stringToSecondDuration(stringDuration)
         local unitNames = unitFinder(rawUnit)
         -- There were no matching units, it's invalid.
         if #unitNames == 0 then
-            -- There were several matching units, which we also can't accept.
-            -- We return the all matched units as the third argument to be used by autocomplete.
             return nil, stringDuration
+        -- There were multiple matching units, which can only happen with Minutes/Months, so we default to Minutes.
         elseif #unitNames > 1 then
-            return nil, stringDuration, unitNames
+            unitNames[1] = "Minute"
         end
         secondValue = secondValue + unitTable[unitNames[1]] * tonumber(rawNum)
     end
-    -- No durations were actually provided, so we return nil
+    -- No durations were actually provided, so return nil.
     if secondValue == -1 then
         -- Add 1 to escape the effect of having -1 as the original value.
         return nil
@@ -62,29 +61,33 @@ local durationType = {
     end;
 
     Autocomplete = function(duration, rawText, isUnitMissingOrMatchedUnits)
+        local returnTable = {}
         if isUnitMissingOrMatchedUnits then
             local unitsTable = isUnitMissingOrMatchedUnits == true and unitFinder("") or isUnitMissingOrMatchedUnits
-            local returnTable = {}
             if isUnitMissingOrMatchedUnits == true then
-                -- Concat the entire unit name to existing text
+                -- Concat the entire unit name to existing text.
                 for i, unit in pairs(unitsTable) do
                     returnTable[i] = rawText .. unit
                 end
             else
-                -- Concat the rest of the unit based on what already exists of the unit name
+                -- Concat the rest of the unit based on what already exists of the unit name.
                 local existingUnitLength = rawText:match("^.*(%a+)$"):len()
                 for i, unit in pairs(unitsTable) do
                     returnTable[i] = rawText .. unit:sub(existingUnitLength + 1)
                 end
             end
-            return returnTable
         elseif duration ~= nil then
             local endingUnit = rawText:match("^.*%d+(%a+)$")
             -- Assume there is a singular match at this point
-            local fuzzyUnit = unitFinder(endingUnit)[1]
-            return {rawText .. fuzzyUnit:sub(#endingUnit + 1)}
+            local fuzzyUnits = unitFinder(endingUnit)
+            -- List all possible fuzzy matches. This is for the Minutes/Months ambiguity case.
+            for i, unit in pairs(fuzzyUnits) do
+                returnTable[i] = rawText .. unit:sub(#endingUnit + 1)
+            end
+            -- Sort alphabetically in the Minutes/Months case, so Minutes are displayed on top.
+            table.sort(returnTable)
         end
-        return {}
+        return returnTable
     end;
 
     Parse = function(duration)
