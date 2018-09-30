3cc30a0a7f4c5d5061ed76220e1d35af67e9a615
Eryn Lynn <eryn@eryn.io>
2018-09-29 20:17:46 -0400

    Fix all autocomplete bugs forever
    Fixes #36

--- Cmdr/CmdrClient/CmdrInterface/Window.lua
+++ Cmdr/CmdrClient/CmdrInterface/Window.lua
@@ -10,11 +10,11 @@ local WINDOW_MAX_HEIGHT = 300
 
 --- Window handles the command bar GUI
 local Window = {
-	Valid = true;
-	AutoComplete = nil;
-	ProcessEntry = nil;
-	OnTextChanged = nil;
-	Cmdr = nil;
+	Valid = true,
+	AutoComplete = nil,
+	ProcessEntry = nil,
+	OnTextChanged = nil,
+	Cmdr = nil
 }
 
 local Gui = Player:WaitForChild("PlayerGui"):WaitForChild("Cmdr"):WaitForChild("Frame")
@@ -24,19 +24,19 @@ local Entry = Gui:WaitForChild("Entry")
 Line.Parent = nil
 
 --- Update the text entry label
-function Window:UpdateLabel ()
+function Window:UpdateLabel()
 	Entry.TextLabel.Text = Player.Name .. "@" .. self.Cmdr.PlaceName .. "$"
 	Entry.TextLabel.Size = UDim2.new(0, Entry.TextLabel.TextBounds.X, 0, 20)
 	Entry.TextBox.Position = UDim2.new(0, Entry.TextLabel.Size.X.Offset + 7, 0, 0)
 end
 
 --- Get the text entry label
-function Window:GetLabel ()
+function Window:GetLabel()
 	return Entry.TextLabel.Text
 end
 
 --- Recalculate the window height
-function Window:UpdateWindowHeight ()
+function Window:UpdateWindowHeight()
 	local numLines = 0
 
 	for _, child in pairs(Gui:GetChildren()) do
@@ -48,13 +48,19 @@ function Window:UpdateWindowHeight ()
 	local windowHeight = (numLines * LINE_HEIGHT) + 20
 
 	Gui.CanvasSize = UDim2.new(Gui.CanvasSize.X.Scale, Gui.CanvasSize.X.Offset, 0, windowHeight)
-	Gui.Size = UDim2.new(Gui.Size.X.Scale, Gui.Size.X.Offset, 0, windowHeight > WINDOW_MAX_HEIGHT and WINDOW_MAX_HEIGHT or windowHeight)
+	Gui.Size =
+		UDim2.new(
+		Gui.Size.X.Scale,
+		Gui.Size.X.Offset,
+		0,
+		windowHeight > WINDOW_MAX_HEIGHT and WINDOW_MAX_HEIGHT or windowHeight
+	)
 
 	Gui.CanvasPosition = Vector2.new(0, math.clamp(windowHeight - 300, 0, math.huge))
 end
 
 --- Add a line to the command bar
-function Window:AddLine (text, color)
+function Window:AddLine(text, color)
 	if #text == 0 then
 		return
 	end
@@ -66,12 +72,12 @@ function Window:AddLine (text, color)
 end
 
 --- Returns if the command bar is visible
-function Window:IsVisible ()
+function Window:IsVisible()
 	return Gui.Visible
 end
 
 --- Sets the command bar visible or not
-function Window:SetVisible (visible)
+function Window:SetVisible(visible)
 	Gui.Visible = visible
 
 	if visible then
@@ -84,17 +90,17 @@ function Window:SetVisible (visible)
 end
 
 --- Hides the command bar
-function Window:Hide ()
+function Window:Hide()
 	return self:SetVisible(false)
 end
 
 --- Shows the command bar
-function Window:Show ()
+function Window:Show()
 	return self:SetVisible(true)
 end
 
 --- Sets the text in the command bar text box, and captures focus
-function Window:SetEntryText (text)
+function Window:SetEntryText(text)
 	Entry.TextBox.Text = text
 
 	if self:IsVisible() then
@@ -103,13 +109,13 @@ function Window:SetEntryText (text)
 end
 
 --- Gets the text in the command bar text box
-function Window:GetEntryText ()
-	return Entry.TextBox.Text
+function Window:GetEntryText()
+	return Entry.TextBox.Text:gsub("\t", "")
 end
 
 --- Sets whether the command is in a valid state or not.
 -- Cannot submit if in invalid state.
-function Window:SetIsValidInput (isValid, errorText)
+function Window:SetIsValidInput(isValid, errorText)
 	Entry.TextBox.TextColor3 = isValid and Color3.fromRGB(255, 255, 255) or Color3.fromRGB(255, 73, 73)
 	self.Valid = isValid
 	self._errorText = errorText
@@ -136,7 +142,7 @@ function Window:LoseFocus(submit)
 end
 
 --- Handles user input when the box is focused
-function Window:BeginInput (input, gameProcessed)
+function Window:BeginInput(input, gameProcessed)
 	if GuiService.MenuIsOpen then
 		self:Hide()
 	end
@@ -170,45 +176,79 @@ function Window:BeginInput (input, gameProcessed)
 		self:SetEntryText(self:GetEntryText():gsub("\n", ""):gsub("\r", ""))
 	elseif input.KeyCode == Enum.KeyCode.Tab then -- Auto complete
 		local item = self.AutoComplete:GetSelectedItem()
-		local text = self:GetEntryText():gsub("\t+", "") -- Get rid of the tab button text input since we use it for auto complete
-		if item and not (text:sub(#text, #text) == " " and self.AutoComplete.LastItem) then
-			local typed = item[1]
+		local text = self:GetEntryText()
+		if item and not (text:sub(#text, #text):match("%s") and self.AutoComplete.LastItem) then
 			local replace = item[2]
+			local newText
+			local insertSpace = true
+			local command = self.AutoComplete.Command
 
-			-- Put auto completion options in quotation marks if they have a space
-			if replace:find(" ") then
-				replace = ('"%s%s"'):format(self.AutoComplete.Prefix, replace)
+			if command then
+				local lastArg = self.AutoComplete.Arg
+
+				newText = command.Alias
+				insertSpace = self.AutoComplete.NumArgs ~= #command.ArgumentDefinitions
+
+				local args = command.Arguments
+				for i = 1, #args do
+					local arg = args[i]
+					local segments = arg.RawSegments
+					if arg == lastArg then
+						segments[#segments] = replace
+					end
+
+					local argText = arg.Prefix .. table.concat(segments, ",")
+
+					-- Put auto completion options in quotation marks if they have a space
+					if argText:find(" ") then
+						argText = ("%q"):format(argText)
+					end
+
+					newText = ("%s %s"):format(newText, argText)
+
+					if arg == lastArg then
+						break
+					end
+				end
 			else
-				replace = self.AutoComplete.Prefix .. replace
+				newText = replace
 			end
-
-			local newText = text:sub(1, #text - #typed - #self.AutoComplete.Prefix) .. replace .. " "
 			-- need to wait a frame so we can eat the \t
 			wait()
 			-- Update the text box
-			self:SetEntryText(newText)
+			self:SetEntryText(newText .. (insertSpace and " " or ""))
 		else
 			-- Still need to eat the \t even if there is no auto-complete to show
 			wait()
-			self:SetEntryText(self:GetEntryText():gsub("\t", ""))
+			self:SetEntryText(self:GetEntryText())
 		end
 	end
 end
 
 -- Hook events
-Entry.TextBox.FocusLost:Connect(function(submit)
-	return Window:LoseFocus(submit)
-end)
-
-UserInputService.InputBegan:Connect(function(input, gameProcessed)
-	return Window:BeginInput(input, gameProcessed)
-end)
-
-Entry.TextBox:GetPropertyChangedSignal("Text"):Connect(function()
-	if Window.OnTextChanged then
-		return Window.OnTextChanged(Entry.TextBox.Text)
+Entry.TextBox.FocusLost:Connect(
+	function(submit)
+		return Window:LoseFocus(submit)
 	end
-end)
+)
+
+UserInputService.InputBegan:Connect(
+	function(input, gameProcessed)
+		return Window:BeginInput(input, gameProcessed)
+	end
+)
+
+Entry.TextBox:GetPropertyChangedSignal("Text"):Connect(
+	function()
+		if Entry.TextBox.Text:match("\t") then -- Eat \t
+			Entry.TextBox.Text = Entry.TextBox.Text:gsub("\t", "")
+			return
+		end
+		if Window.OnTextChanged then
+			return Window.OnTextChanged(Entry.TextBox.Text)
+		end
+	end
+)
 
 Gui.ChildAdded:Connect(Window.UpdateWindowHeight)
 
