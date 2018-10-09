9ecb332c70fae38a5eb65fc7564e8ad6fbf3c9d8
Michael Haas <TripleFrequency@TripleFrequency.com>
2018-10-09 18:07:53 -0400

    Fix style issues in clear command

--- Cmdr/BuiltInCommands/clear.lua
+++ Cmdr/BuiltInCommands/clear.lua
@@ -1,27 +1,26 @@
 local Players = game:GetService("Players")
 
 return {
-	Name = "clear";
-	Aliases = {};
-	Description = "Clear all lines above the entry line of the Cmdr window.";
-	Group = "DefaultUtil";
-	Args = {};
+	Name = "clear",
+	Aliases = {},
+	Description = "Clear all lines above the entry line of the Cmdr window.",
+	Group = "DefaultUtil",
+	Args = {},
+	Run = function()
+		local player = Players.LocalPlayer
+		local gui = player:WaitForChild("PlayerGui"):WaitForChild("Cmdr")
+		local frame = gui:WaitForChild("Frame")
 
-    Run = function()
-        local Player = Players.LocalPlayer
-        local Gui = Player:WaitForChild("PlayerGui"):WaitForChild("Cmdr")
-        local Frame = Gui:WaitForChild("Frame")
-
-        if Gui and Frame then
-            local shrinkHeight = 0
-            for _, child in pairs(Frame:GetChildren()) do
-                if (child.Name == "Line" and child:IsA("TextLabel")) then
-                    shrinkHeight = shrinkHeight + child.Size.Y.Offset
-                    child:Destroy()
-                end
-            end
-            Frame.Size = Frame.Size - UDim2.new(0, 0, 0, -shrinkHeight)
-        end
+		if gui and frame then
+			local shrinkHeight = 0
+			for _, child in pairs(frame:GetChildren()) do
+				if child.Name == "Line" and child:IsA("TextLabel") then
+					shrinkHeight = shrinkHeight + child.Size.Y.Offset
+					child:Destroy()
+				end
+			end
+			frame.Size = frame.Size - UDim2.new(0, 0, 0, -shrinkHeight)
+		end
 		return ""
 	end
 }
