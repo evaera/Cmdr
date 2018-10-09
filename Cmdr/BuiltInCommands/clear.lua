bc8d31ea0ccdcfee5085035598cfe4a766d298f3
Michael Haas <TripleFrequency@TripleFrequency.com>
2018-10-09 01:03:54 -0400

    Add the client-side clear command

---
+++ Cmdr/BuiltInCommands/clear.lua
@@ -0,0 +1,27 @@
+local Players = game:GetService("Players")
+
+return {
+	Name = "clear";
+	Aliases = {};
+	Description = "Clear all lines above the entry line of the Cmdr window.";
+	Group = "DefaultUtil";
+	Args = {};
+
+    Run = function()
+        local Player = Players.LocalPlayer
+        local Gui = Player:WaitForChild("PlayerGui"):WaitForChild("Cmdr")
+        local Frame = Gui:WaitForChild("Frame")
+
+        if Gui and Frame then
+            local shrinkHeight = 0
+            for _, child in pairs(Frame:GetChildren()) do
+                if (child.Name == "Line" and child:IsA("TextLabel")) then
+                    shrinkHeight = shrinkHeight + child.Size.Y.Offset
+                    child:Destroy()
+                end
+            end
+            Frame.Size = Frame.Size - UDim2.new(0, 0, 0, -shrinkHeight)
+        end
+		return nil
+	end
+}
