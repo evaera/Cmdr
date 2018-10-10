91ce64153569b4a522a157587ec22f2818451d99
Michael Haas <TripleFrequency@TripleFrequency.com>
2018-10-10 12:19:44 -0400

    Recalculate window size when nothing is returned
    This also removes unecessary code from the clear command, and fixes an issue where the window would expand instead of shrinking when running the clear command.

--- Cmdr/BuiltInCommands/clear.lua
+++ Cmdr/BuiltInCommands/clear.lua
@@ -12,14 +12,11 @@ return {
 		local frame = gui:WaitForChild("Frame")
 
 		if gui and frame then
-			local shrinkHeight = 0
 			for _, child in pairs(frame:GetChildren()) do
 				if child.Name == "Line" and child:IsA("TextLabel") then
-					shrinkHeight = shrinkHeight + child.Size.Y.Offset
 					child:Destroy()
 				end
 			end
-			frame.Size = frame.Size - UDim2.new(0, 0, 0, -shrinkHeight)
 		end
 		return ""
 	end
