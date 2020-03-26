5bd189c0f4b4a6eb26cb7985badb7204389f2bde
Jaguar <j.aguar.515.515@gmail.com>
2020-03-25 18:00:57 -0700

    Removed comments

--- Cmdr/BuiltInCommands/Admin/respawnServer.lua
+++ Cmdr/BuiltInCommands/Admin/respawnServer.lua
@@ -1,9 +1,3 @@
--- A fully extensible and type-safe admin-commands console
--- @documentation https://github.com/evaera/Cmdr/blob/master/README.md
--- @source https://github.com/evaera/Cmdr
--- @rostrap Cmdr
--- @author evaera
-
 return function(_, players)
 	for _, player in pairs(players) do
 		if player.Character then
