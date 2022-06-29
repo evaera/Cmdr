033b9ea2ff22b09e0526c8b43c9a8efe97cbe0d4
Nidoxs <nidoxsdevelopment@gmail.com>
2022-06-29 17:18:33 +0100

    Create :SetMaxWindowHeight() method in CmdrClient init

--- Cmdr/CmdrClient/init.lua
+++ Cmdr/CmdrClient/init.lua
@@ -6,33 +6,37 @@ local Shared = script:WaitForChild("Shared")
 local Util = require(Shared:WaitForChild("Util"))
 
 if RunService:IsClient() == false then
-	error("Server scripts cannot require the client library. Please require the server library to use Cmdr in your own code.")
+	error(
+		"Server scripts cannot require the client library. Please require the server library to use Cmdr in your own code."
+	)
 end
 
-local Cmdr do
+local Cmdr
+do
 	Cmdr = setmetatable({
-		ReplicatedRoot = script;
-		RemoteFunction = script:WaitForChild("CmdrFunction");
-		RemoteEvent = script:WaitForChild("CmdrEvent");
-		ActivationKeys = {[Enum.KeyCode.F2] = true};
-		Enabled = true;
-		MashToEnable = false;
-		ActivationUnlocksMouse = false;
-		HideOnLostFocus = true;
-		PlaceName = "Cmdr";
-		Util = Util;
-		Events = {};
+		ReplicatedRoot = script,
+		RemoteFunction = script:WaitForChild("CmdrFunction"),
+		RemoteEvent = script:WaitForChild("CmdrEvent"),
+		ActivationKeys = { [Enum.KeyCode.F2] = true },
+		Enabled = true,
+		MashToEnable = false,
+		ActivationUnlocksMouse = false,
+		HideOnLostFocus = true,
+		PlaceName = "Cmdr",
+		Util = Util,
+		MaxWindowHeight = 300,
+		Events = {},
 	}, {
 		-- This sucks, and may be redone or removed
 		-- Proxies dispatch methods on to main Cmdr object
-		__index = function (self, k)
+		__index = function(self, k)
 			local r = self.Dispatcher[k]
 			if r and type(r) == "function" then
-				return function (_, ...)
+				return function(_, ...)
 					return r(self.Dispatcher, ...)
 				end
 			end
-		end
+		end,
 	})
 
 	Cmdr.Registry = require(Shared.Registry)(Cmdr)
@@ -46,28 +50,42 @@ end
 local Interface = require(script.CmdrInterface)(Cmdr)
 
 --- Sets a list of keyboard keys (Enum.KeyCode) that can be used to open the commands menu
-function Cmdr:SetActivationKeys (keysArray)
+function Cmdr:SetActivationKeys(keysArray)
 	self.ActivationKeys = Util.MakeDictionary(keysArray)
 end
 
+--- Sets the max window height of the interface
+function Cmdr:SetMaxWindowHeight(newMaxWindowHeight)
+	local camera = workspace:FindFirstChildWhichIsA("Camera")
+
+	if camera then
+		local ySize = camera.ViewportSize.Y
+		self.MaxWindowHeight = math.clamp(newMaxWindowHeight, 150, ySize - 25)
+	else
+		self.MaxWindowHeight = 300
+	end
+
+	Interface.Window:UpdateMaxHeight()
+end
+
 --- Sets the place name label on the interface
-function Cmdr:SetPlaceName (name)
+function Cmdr:SetPlaceName(name)
 	self.PlaceName = name
 	Interface.Window:UpdateLabel()
 end
 
 --- Sets whether or not the console is enabled
-function Cmdr:SetEnabled (enabled)
+function Cmdr:SetEnabled(enabled)
 	self.Enabled = enabled
 end
 
 --- Sets if activation will free the mouse.
-function Cmdr:SetActivationUnlocksMouse (enabled)
+function Cmdr:SetActivationUnlocksMouse(enabled)
 	self.ActivationUnlocksMouse = enabled
 end
 
 --- Shows Cmdr window
-function Cmdr:Show ()
+function Cmdr:Show()
 	if not self.Enabled then
 		return
 	end
@@ -76,12 +94,12 @@ function Cmdr:Show ()
 end
 
 --- Hides Cmdr window
-function Cmdr:Hide ()
+function Cmdr:Hide()
 	Interface.Window:Hide()
 end
 
 --- Toggles Cmdr window
-function Cmdr:Toggle ()
+function Cmdr:Toggle()
 	if not self.Enabled then
 		return self:Hide()
 	end
