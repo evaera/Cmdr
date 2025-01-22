714148f2f5ce9bddf68809dce7d123b23218f938
Arthur <41109533+brennanarthurisaac@users.noreply.github.com>
2025-01-22 22:49:28 +0000

    Migrate to TextChatService:CanUsersChatAsync

--- Cmdr/BuiltInCommands/Admin/announceServer.lua
+++ Cmdr/BuiltInCommands/Admin/announceServer.lua
@@ -1,12 +1,12 @@
+local TextChatService = game:GetService("TextChatService")
 local TextService = game:GetService("TextService")
 local Players = game:GetService("Players")
-local Chat = game:GetService("Chat")
 
 return function(context, text)
 	local filterResult = TextService:FilterStringAsync(text, context.Executor.UserId, Enum.TextFilterContext.PublicChat)
 
 	for _, player in ipairs(Players:GetPlayers()) do
-		if Chat:CanUsersChatAsync(context.Executor.UserId, player.UserId) then
+		if TextChatService:CanUsersChatAsync(context.Executor.UserId, player.UserId) then
 			context:SendEvent(player, "Message", filterResult:GetChatForUserAsync(player.UserId), context.Executor)
 		end
 	end
