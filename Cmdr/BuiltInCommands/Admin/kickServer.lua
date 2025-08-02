91085d0d15a2d4bcceec771e99018554f6fa9f10
LeAndre <lcjunior1220@gmail.com>
2025-08-02 14:43:58 -0400

    Update kickServer.lua

--- Cmdr/BuiltInCommands/Admin/kickServer.lua
+++ Cmdr/BuiltInCommands/Admin/kickServer.lua
@@ -1,6 +1,7 @@
-return function(_, players)
+return function(_, players, reason)
+	reason = if reason then reason else `Kicked by {_.Executor.Name}.`
 	for _, player in pairs(players) do
-		player:Kick("Kicked by admin.")
+		player:Kick(reason)
 	end
 
 	return ("Kicked %d players."):format(#players)
