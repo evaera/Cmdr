07a332211139f2056c78e314880981a26c501353
LeAndre <lcjunior1220@gmail.com>
2025-08-11 23:24:20 -0400

    Update kickServer.lua

--- Cmdr/BuiltInCommands/Admin/kickServer.lua
+++ Cmdr/BuiltInCommands/Admin/kickServer.lua
@@ -1,5 +1,6 @@
+local Chat = game:GetService("Chat")
 return function(_, players, reason)
-	reason = if reason then reason else `Kicked by {_.Executor.Name}.`
+	reason = if reason then Chat:FilterStringForBroadcastAsync(_.Executor, reason) else `Kicked by {_.Executor.Name}.`
 	for _, player in pairs(players) do
 		player:Kick(reason)
 	end
