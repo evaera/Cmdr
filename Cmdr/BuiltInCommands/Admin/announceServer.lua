b4141b346676def24ddad77d2bac38e2b2325b43
Hakai <170677187+ShadowDaughter@users.noreply.github.com>
2025-11-03 05:32:53 +0100

    Reimplement chat filtering

--- Cmdr/BuiltInCommands/Admin/announceServer.lua
+++ Cmdr/BuiltInCommands/Admin/announceServer.lua
@@ -1,12 +1,23 @@
 local TextChatService = game:GetService("TextChatService")
+local TextService = game:GetService("TextService")
 local Players = game:GetService("Players")
 
 return function(context, text)
-	for _, player in ipairs(Players:GetPlayers()) do
-		if TextChatService:CanUsersChatAsync(context.Executor.UserId, player.UserId) then
-			context:SendEvent(player, "Message", text)
+	local success, filterResult = pcall(function(...)
+		return TextService:FilterStringAsync(text, context.Executor.UserId, Enum.TextFilterContext.PublicChat)
+	end)
+
+	if success then
+		local filteredText = filterResult:GetNonChatStringForBroadcastAsync()
+
+		for _, player in ipairs(Players:GetPlayers()) do
+			if TextChatService:CanUsersChatAsync(context.Executor.UserId, player.UserId) then
+				context:SendEvent(player, "Message", filteredText)
+			end
 		end
+
+		return "Created announcement."
 	end
 
-	return "Created announcement."
+	return "Failed to filter announcement!"
 end
