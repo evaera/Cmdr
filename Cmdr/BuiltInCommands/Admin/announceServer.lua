d416321348770018fd74dee4f892a86323a0c450
Hakai <170677187+ShadowDaughter@users.noreply.github.com>
2025-11-03 04:18:07 +0100

    Migrate announceServer and DefaultEventHandlers for TextChatService support

--- Cmdr/BuiltInCommands/Admin/announceServer.lua
+++ Cmdr/BuiltInCommands/Admin/announceServer.lua
@@ -1,13 +1,10 @@
 local TextChatService = game:GetService("TextChatService")
-local TextService = game:GetService("TextService")
 local Players = game:GetService("Players")
 
 return function(context, text)
-	local filterResult = TextService:FilterStringAsync(text, context.Executor.UserId, Enum.TextFilterContext.PublicChat)
-
 	for _, player in ipairs(Players:GetPlayers()) do
 		if TextChatService:CanUsersChatAsync(context.Executor.UserId, player.UserId) then
-			context:SendEvent(player, "Message", filterResult:GetChatForUserAsync(player.UserId), context.Executor)
+			context:SendEvent(player, "Message", text)
 		end
 	end
 
