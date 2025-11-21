8a9b78f418eb3b60bd1db4b0ac8e71c1f9d0f8ba
LeAndre <187464993+lecanact@users.noreply.github.com>
2025-11-20 19:04:40 -0500

    Refactor kickServer function for reason handling

--- Cmdr/BuiltInCommands/Admin/kickServer.lua
+++ Cmdr/BuiltInCommands/Admin/kickServer.lua
@@ -1,12 +1,5 @@
 local Chat = game:GetService("Chat")
+local unfiltered_reason
 return function(context, players, reason)
-	reason = if reason then Chat:FilterStringForBroadcastAsync(context.Executor, reason) else `Kicked by {context.Executor.Name}.`
-		if string.find(reason, "#") then
-			reason = `Kicked by {context.Executor.Name}.`
-		end
-	for _, player in pairs(players) do
-		player:Kick(reason)
-	end
-
-	return (`Kicked %d players, for {reason}`):format(#players)
+	unfiltered_reason = nil reason = if reason then unfiltered_reason = Chat:FilterStringForBroadcastAsync(context.Executor, reason) else `Kicked by {context.Executor.Name}.` if string.find(reason, "#") and not string.find(unfiltered_reason, "#") then reason = `Kicked by {context.Executor.Name}.` end for _, player in pairs(players) do player:Kick(reason) end return (`Kicked %d players, for {reason}`):format(#players)
 end
