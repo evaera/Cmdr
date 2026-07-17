cc8fb917d43bb508d5592fdad0f2c65fafd8c878
Wil <63251107+wilyt1@users.noreply.github.com>
2026-07-17 18:43:58 -0400

    Handle empty strings

--- Cmdr/BuiltInCommands/Admin/banServer.lua
+++ Cmdr/BuiltInCommands/Admin/banServer.lua
@@ -14,23 +14,31 @@ return function(
 		return "Too many players specified. BanAsync limits batches to a maximum of 50 players."
 	end
 
-	if duration < 1 and duration ~= -1 then
-		duration = -1
+	if displayReason:match("^%s*$") then
+		return "Display reason cannot be empty."
+	end
+
+	if not privateReason or privateReason:match("^%s*$") then
+		privateReason = displayReason
 	end
 
 	if #displayReason > 400 then
 		return "Display reason exceeds the 400-character limit."
 	end
 
-	if privateReason and #privateReason > 1000 then
+	if #privateReason > 1000 then
 		return "Private reason exceeds the 1000-character limit."
+	end
+
+	if duration < 1 and duration ~= -1 then
+		duration = -1
 	end
 
 	local success, errorMessage = pcall(Players.BanAsync, Players, {
 		UserIds = playerIds,
 		Duration = duration,
 		DisplayReason = displayReason,
-		PrivateReason = privateReason or displayReason,
+		PrivateReason = privateReason,
 		ExcludeAltAccounts = excludeAltAccounts,
 		ApplyToUniverse = applyToUniverse,
 		ApplyDeviceBlock = applyDeviceBlock,
